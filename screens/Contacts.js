9fbcc3fe17fc9b6efaba44f73f1270fe5a6ff913
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-11-03 13:43:02 -0300

    contacts

--- screens/Contacts.js
+++ screens/Contacts.js
@@ -2,7 +2,7 @@ import { View, Text, FlatList } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import useContacts from '../hooks/useHooks';
 import GlobalContext from '../context/Context';
-import { collection, query, where } from 'firebase/firestore';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { db } from '../firebase';
 
 const Contacts = () => {
@@ -23,10 +23,16 @@ const ContactPreview = ({contact, image}) => {
   const [user, setUser] = useState(contact)
   useEffect(() => {
     const q = query(collection(db, "users"), where('email', '==', contact.email))
-    // unsubscribe
-    
-    // 2:59:00
+    const unsubscribe =  onSnapshot(q, snapshot => {
+      if(snapshot.docs.length){
+        const userDoc = snapshot.docs[0].data();
+        setUser(prevUser => ({...prevUser, userDoc}))
+      }
+    })
+    return () => unsubscribe();
   }, [])
 }
 
 export default Contacts;
+
+// 2:59:00
